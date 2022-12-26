70502ab082c5ab4e11f619ef2b9de54774f55e93
sepiggy <jms1209@qq.com>
2022-12-26 17:28:32 +0800

    b4 p14 ok

--- chapter1/src/main/java/nia/chapter1/ConnectExample.java
+++ chapter1/src/main/java/nia/chapter1/ConnectExample.java
@@ -8,48 +8,48 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.net.InetSocketAddress;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by kerr.
- *
+ * <p>
  * 代码清单 1-3 异步地建立连接
- *
+ * <p>
  * 代码清单 1-4 回调实战
  */
 public class ConnectExample {
+
     private static final Channel CHANNEL_FROM_SOMEWHERE = new NioSocketChannel();
 
     /**
      * 代码清单 1-3 异步地建立连接
-     *
+     * <p>
      * 代码清单 1-4 回调实战
-     * */
+     */
     public static void connect() {
-        Channel channel = CHANNEL_FROM_SOMEWHERE; //reference form somewhere
+        Channel channel = CHANNEL_FROM_SOMEWHERE; // reference form somewhere
         // Does not block
-        //异步地连接到远程节点
+        // 异步地连接到远程节点
         ChannelFuture future = channel.connect(
                 new InetSocketAddress("192.168.0.1", 25));
-        //注册一个 ChannelFutureListener，以便在操作完成时获得通知
+        // 注册一个ChannelFutureListener，以便在操作完成时获得通知
         future.addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture future) {
-                //检查操作的状态
+                // 检查操作的状态
                 if (future.isSuccess()) {
-                    //如果操作是成功的，则创建一个 ByteBuf 以持有数据
-                    ByteBuf buffer = Unpooled.copiedBuffer(
-                            "Hello", Charset.defaultCharset());
-                    //将数据异步地发送到远程节点。返回一个 ChannelFuture
-                    ChannelFuture wf = future.channel()
-                            .writeAndFlush(buffer);
+                    // 如果操作是成功的，则创建一个ByteBuf以持有数据
+                    ByteBuf buffer = Unpooled.copiedBuffer("Hello", StandardCharsets.UTF_8);
+                    // 将数据异步地发送到远程节点。返回一个ChannelFuture
+                    ChannelFuture wf = future.channel().writeAndFlush(buffer);
                     // ...
                 } else {
-                    //如果发生错误，则访问描述原因的 Throwable
+                    // 如果发生错误，则访问描述原因的Throwable
                     Throwable cause = future.cause();
                     cause.printStackTrace();
                 }
             }
         });
     }
+
 }
--- chapter1/src/main/java/nia/chapter1/ConnectHandler.java
+++ chapter1/src/main/java/nia/chapter1/ConnectHandler.java
@@ -5,10 +5,11 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 
 /**
  * Created by kerr.
- *
+ * <p>
  * 代码清单 1-2 被回调触发的 ChannelHandler
  */
 public class ConnectHandler extends ChannelInboundHandlerAdapter {
+
     @Override
     //当一个新的连接已经被建立时，channelActive(ChannelHandlerContext)将会被调用
     public void channelActive(ChannelHandlerContext ctx)
@@ -16,4 +17,5 @@ public class ConnectHandler extends ChannelInboundHandlerAdapter {
         System.out.println(
                 "Client " + ctx.channel().remoteAddress() + " connected");
     }
+
 }
